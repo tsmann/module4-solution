874bd4e897512388b0ce0546fd10229b5492ca4a
tsmann <47868327+tsmann@users.noreply.github.com>
2019-04-25 11:07:08 -0400

    Update SpeakHello.js

--- js/SpeakHello.js
+++ js/SpeakHello.js
@@ -2,7 +2,7 @@
 // See Lecture 52, part 2
 (function (window) {
   var SpeakHello = {};
-  SpeakHello.name = "Yaakov" "Paul" "Frank" "Larry" "Paula" "Laura";
+  SpeakHello.name = "Yaakov","Paul","Frank","Larry","Paula","Laura";
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
