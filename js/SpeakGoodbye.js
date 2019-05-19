900c3662640fcc8114fc84113c5bbf8a84cf0441
tsmann <47868327+tsmann@users.noreply.github.com>
2019-05-18 22:51:16 -0400

    Update SpeakGoodbye.js

--- js/SpeakGoodbye.js
+++ js/SpeakGoodbye.js
@@ -1,1 +1,9 @@
+(function (window) {
+  var byeSpeaker = {};
+  byeSpeaker.speak = function (names) {
+var speakWord = "Good Bye";
+  console.log(speakWord + " " + names);
+}
+ window.byeSpeaker = byeSpeaker;
+  })(window);
 
