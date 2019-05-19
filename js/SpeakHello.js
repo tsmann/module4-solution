b5574b54c5e90b8a244629dafdbef9c8f778f2d1
tsmann <47868327+tsmann@users.noreply.github.com>
2019-05-18 22:31:35 -0400

    Delete SpeakHello.js

--- js/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-  var helloSpeaker = {};
-  helloSpeaker.speak = function (names) {
-  var speakWord = "Hello";
-        console.log(speakWord + " " + names);
-    }
-  window.helloSpeaker = helloSpeaker;
-    })(window);
