6c610219d86daa318673a5abe79d48fa5ce2f3be
tsmann <47868327+tsmann@users.noreply.github.com>
2019-05-18 22:31:18 -0400

    Delete SpeakGoodBye.js

--- js/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-  var byeSpeaker = {};
-  byeSpeaker.speak = function (names) {
-var speakWord = "Good Bye";
-  console.log(speakWord + " " + names);
-}
- window.byeSpeaker = byeSpeaker;
-  })(window);
