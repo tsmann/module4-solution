cb4f10eac2093671e2601d9b57de2327c0540438
tsmann <47868327+tsmann@users.noreply.github.com>
2019-05-18 22:52:18 -0400

    Update SpeakHello.js

--- js/SpeakHello.js
+++ js/SpeakHello.js
@@ -1,1 +1,10 @@
+(function (window) {
+  var helloSpeaker = {};
+  helloSpeaker.speak = function (name) {
+    var speakWord = "Hello";
+    //var helloSpeaker = {};
+        console.log(speakWord + " " + name);
+    }
+  window.helloSpeaker = helloSpeaker;
+    })(window);
 
